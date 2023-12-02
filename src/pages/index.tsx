d3f34af219456d3bd5413d2450de3000020798e3
Jonas Thuvesson Winberg <jonasthuvesson@gmail.com>
2023-12-02 18:57:35 +0100

    Make window change dynamic

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
 import Snowflake from "@/components/snowflake";
-import { ReactElement, useRef } from "react";
+import { ReactElement, useEffect, useRef, useState } from "react";
 import WindowGrid from "@/components/window-grid";
 import NoSsr from "@/components/no-ssr";
 import {
@@ -14,15 +14,44 @@ import { getRandomInt } from "@/util/get-random-int";
 
 const inter = Inter({ subsets: ["latin"] });
 
-const maxW = 4600;
-const maxH = 3300;
-
 export interface AudioHandlers {
   pauseMusic: () => void;
   playMusic: () => void;
 }
 
 export default function Home() {
+  const [width, setWidth] = useState(1000);
+  const [height, setHeight] = useState(1000);
+
+  useEffect(() => {
+    window.addEventListener("resize", calibrateHeight);
+    calibrateHeight();
+  }, []);
+
+  const calibrateHeight = () => {
+    console.log("calibrating");
+    const body = document.body;
+    const html = document.documentElement;
+
+    const height = Math.max(
+      body.scrollHeight,
+      body.offsetHeight,
+      html.clientHeight,
+      html.scrollHeight,
+      html.offsetHeight
+    );
+
+    const width = Math.max(
+      body.scrollWidth,
+      body.offsetWidth,
+      html.clientWidth,
+      html.offsetWidth
+    );
+
+    setHeight(height);
+    setWidth(width);
+  };
+
   const [santaStyle, santaAnimRef] = useSpring(() => ({
     config: config.slow,
     from: { opacity: 1, transform: "rotate(0turn)" },
@@ -84,7 +113,7 @@ export default function Home() {
   for (let i = 0; i < 200; i++) {
     const size = getRandomInt(10, 3);
     const speed = getRandomInt(15, 3);
-    const horizontal = getRandomInt(maxW, 0);
+    const horizontal = getRandomInt(width, 0);
     const delay = getRandomInt(800, 0);
     snowFlakes.push(
       <Snowflake
@@ -115,20 +144,19 @@ export default function Home() {
       </animated.div>
       <audio ref={audioElem} src="jingle-bells.mp3" loop={true}></audio>
       <main
-        style={{ maxWidth: maxW + "px", maxHeight: maxH + "px" }}
-        className={`lg:h-[100vh] h-[95vh] w-screen`}
+        style={{ height: height + "px", width: width + "px" }}
+        className={`w-screen`}
       >
         <div
           style={{
-            maxWidth: maxW + "px",
-            maxHeight: maxH + "px",
+            // width: maxW + "px",
             pointerEvents: "none",
           }}
-          className="z-1 w-full lg:h-[100vh] h-[90vh] md:h-[95vh] absolute overflow-x-hidden overflow-y-hidden"
+          className="z-1 w-full h-full absolute overflow-x-hidden overflow-y-hidden"
         >
           {snowFlakes}
 
-          <div className="flex flex-col justify-center items-center h-screen w-screen max-w-full max-h-full">
+          <div className="flex flex-col justify-center items-center w-full h-full">
             <h1
               style={{
                 fontFamily: "Smooch, cursive",
